bbf9a1a61239e9594208c73acf3431e354fbef97
Daniel Piron <dannypiron@gmail.com>
2021-02-18 23:50:48 -0500

    USe doublequotes througout for consistency with merge target

--- 44 Hangman/python/hangman.py
+++ 44 Hangman/python/hangman.py
@@ -7,19 +7,19 @@ import random
 
 
 class Canvas:
-    ''' For drawing text-based figures '''
+    """ For drawing text-based figures """
 
-    def __init__(self, width=12, height=12, fill=' '):
+    def __init__(self, width=12, height=12, fill=" "):
         self._buffer = []
         for _ in range(height):
             line = []
             for _ in range(width):
-                line.append('')
+                line.append("")
             self._buffer.append(line)
 
         self.clear()
 
-    def clear(self, fill=' '):
+    def clear(self, fill=" "):
         for row in self._buffer:
             for x in range(len(row)):
                 row[x] = fill
@@ -27,10 +27,10 @@ class Canvas:
     def render(self):
         lines = []
         for line  in self._buffer:
-            # Joining by the empty string ('') smooshes all of the
+            # Joining by the empty string ("") smooshes all of the
             # individual characters together as one line.
-            lines.append(''.join(line))
-        return '\n'.join(lines)
+            lines.append("".join(line))
+        return "\n".join(lines)
 
     def put(self, s, x, y):
         # In an effort to avoid distorting the drawn image, only write the
@@ -40,68 +40,68 @@ class Canvas:
 
 def draw_gallows(canvas):
     for i in range(12):
-        canvas.put('X', 0, i)
+        canvas.put("X", 0, i)
     for i in range(7):
-        canvas.put('X', i, 0)
-    canvas.put('X', 6, 1)
+        canvas.put("X", i, 0)
+    canvas.put("X", 6, 1)
 
 
 def draw_head(canvas):
-    canvas.put('-', 5, 2)
-    canvas.put('-', 6, 2)
-    canvas.put('-', 7, 2)
-    canvas.put('(', 4, 3)
-    canvas.put('.', 5, 3)
-    canvas.put('.', 7, 3)
-    canvas.put(')', 8, 3)
-    canvas.put('-', 5, 4)
-    canvas.put('-', 6, 4)
-    canvas.put('-', 7, 4)
+    canvas.put("-", 5, 2)
+    canvas.put("-", 6, 2)
+    canvas.put("-", 7, 2)
+    canvas.put("(", 4, 3)
+    canvas.put(".", 5, 3)
+    canvas.put(".", 7, 3)
+    canvas.put(")", 8, 3)
+    canvas.put("-", 5, 4)
+    canvas.put("-", 6, 4)
+    canvas.put("-", 7, 4)
 
 
 def draw_body(canvas):
     for i in range(5, 9, 1):
-        canvas.put('X', 6, i)
+        canvas.put("X", 6, i)
 
 
 def draw_right_arm(canvas):
     for i in range(3, 7):
-        canvas.put('\\', i - 1, i)
+        canvas.put("\\", i - 1, i)
 
 
 def draw_left_arm(canvas):
-    canvas.put('/', 10, 3)
-    canvas.put('/', 9, 4)
-    canvas.put('/', 8, 5)
-    canvas.put('/', 7, 6)
+    canvas.put("/", 10, 3)
+    canvas.put("/", 9, 4)
+    canvas.put("/", 8, 5)
+    canvas.put("/", 7, 6)
 
 
 def draw_right_leg(canvas):
-    canvas.put('/', 5, 9)
-    canvas.put('/', 4, 10)
+    canvas.put("/", 5, 9)
+    canvas.put("/", 4, 10)
 
 
 def draw_left_leg(canvas):
-    canvas.put('\\', 7, 9)
-    canvas.put('\\', 8, 10)
+    canvas.put("\\", 7, 9)
+    canvas.put("\\", 8, 10)
 
 
 def draw_left_hand(canvas):
-    canvas.put('\\', 10, 2)
+    canvas.put("\\", 10, 2)
 
 
 def draw_right_hand(canvas):
-    canvas.put('/', 2, 2)
+    canvas.put("/", 2, 2)
 
 
 def draw_left_foot(canvas):
-    canvas.put('\\', 9, 11)
-    canvas.put('-', 10, 11)
+    canvas.put("\\", 9, 11)
+    canvas.put("-", 10, 11)
 
 
 def draw_right_foot(canvas):
-    canvas.put('-', 2, 11)
-    canvas.put('/', 3, 11)
+    canvas.put("-", 2, 11)
+    canvas.put("/", 3, 11)
 
 
 PHASES = (
