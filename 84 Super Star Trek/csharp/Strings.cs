e524af89c581ca12b015d8277043e907066ccbb1
Andrew Cooper <andrew.cooper@nearmap.com>
2021-02-20 23:41:05 +1100

    Add string resources

---
+++ 84 Super Star Trek/csharp/Strings.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using static System.StringComparison;
+
+namespace SuperStarTrek
+{
+    internal static class Strings
+    {
+        public static string Title => GetResource();
+        public static string Instructions => GetResource();
+        public static string Enterprise => GetResource();
+
+        private static string GetResource([CallerMemberName] string name = "")
+        {
+            var streamName = $"SuperStarTrek.Resources.{name}.txt";
+            using var stream = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream(streamName);
+            using var reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
+        }
+    }
+}
