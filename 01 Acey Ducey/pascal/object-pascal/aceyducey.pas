cb430380fef9614e26783a9dcdf49f101b9ae27d
Gustavo Carreno <guscarreno@gmail.com>
2021-02-27 22:24:47 +0000

    Pascal: Adding aceyducey

---
+++ 01 Acey Ducey/pascal/object-pascal/aceyducey.pas
@@ -0,0 +1,18 @@
+program aceyducey;
+
+{$IFDEF FPC}
+{$mode objfpc}{$H+}
+{$ENDIF}
+
+uses
+  Game, Deck
+;
+
+var
+  Acey_Ducey: TGame;
+
+begin
+  Acey_Ducey:= TGame.Create;
+  Acey_Ducey.Run;
+end.
+
