7ab5cfd140e57d227b795e7b988c5ab9be0fe0d6
Jamie McCarthy <jamie.mccarthy@voxmedia.com>
2021-03-01 10:20:40 -0600

    Ruby names constants with UPPER_CASE by convention

--- 51 Hurkle/ruby/hurkle.rb
+++ 51 Hurkle/ruby/hurkle.rb
@@ -1,11 +1,14 @@
+MAX_GUESSES = 5
+GRID_SIZE = 10
+
 def main
   say_introduction
 
   loop do
-    $a = rand($g).floor
-    $b = rand($g).floor
+    $a = rand(GRID_SIZE).floor
+    $b = rand(GRID_SIZE).floor
     found = false
-    (1..$n).each do |k|
+    (1..MAX_GUESSES).each do |k|
       print "GUESS # " + k.to_s + " "
       print "? "
       x, y = gets.chomp.split(",").map(&:to_i)
@@ -26,16 +29,14 @@ def say_introduction
   puts " " * 33 + "HURKLE"
   puts " " * 15 + "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
   3.times { puts }
-  $n = 5
-  $g = 10
   puts
-  puts "A HURKLE IS HIDING ON A " + $g.to_s + " BY " + $g.to_s + " GRID. HOMEBASE"
+  puts "A HURKLE IS HIDING ON A " + GRID_SIZE.to_s + " BY " + GRID_SIZE.to_s + " GRID. HOMEBASE"
   puts "ON THE GRID IS POINT 0,0 IN THE SOUTHWEST CORNER,"
   puts "AND ANY POINT ON THE GRID IS DESIGNATED BY A"
   puts "PAIR OF WHOLE NUMBERS SEPERATED BY A COMMA. THE FIRST"
   puts "NUMBER IS THE HORIZONTAL POSITION AND THE SECOND NUMBER"
   puts "IS THE VERTICAL POSITION. YOU MUST TRY TO"
-  puts "GUESS THE HURKLE'S GRIDPOINT. YOU GET " + $n.to_s + " TRIES."
+  puts "GUESS THE HURKLE'S GRIDPOINT. YOU GET " + MAX_GUESSES.to_s + " TRIES."
   puts "AFTER EACH TRY, I WILL TELL YOU THE APPROXIMATE"
   puts "DIRECTION TO GO TO LOOK FOR THE HURKLE."
   puts
@@ -55,7 +56,7 @@ end
 
 def say_failure
   puts
-  puts "SORRY, THAT'S " + $n.to_s + " GUESSES."
+  puts "SORRY, THAT'S " + MAX_GUESSES.to_s + " GUESSES."
   puts "THE HURKLE IS AT " + $a.to_s + "," + $b.to_s
 end
 
