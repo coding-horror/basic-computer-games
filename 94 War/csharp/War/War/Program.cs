2bb6bff9b87e2f1549a83879feaefc3fbbe6da93
rbamforth <79797573+rbamforth@users.noreply.github.com>
2021-03-22 11:59:50 +0000

    Added initial files
    Added new Visual Studio solution.

---
+++ 94 War/csharp/War/War/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace War
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
