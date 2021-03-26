2254e95268af5ecacca400230ba206b64d4bb16b
rbamforth <79797573+rbamforth@users.noreply.github.com>
2021-03-26 22:51:28 +0000

    Moved UI stuff into its own class. Minor changes, commenting and tidying up the other classes.

--- 94 War/csharp/War/War/Program.cs
+++ 94 War/csharp/War/War/Program.cs
@@ -1,58 +1,11 @@
-﻿using System;
-
-namespace War
+﻿namespace War
 {
-	public class Intro
-	{
-		public void WriteIntro()
-		{
-			Console.WriteLine("                                 WAR");
-			Console.WriteLine("               CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY");
-			Console.WriteLine();
-			Console.WriteLine();
-			Console.WriteLine();
-	
-			Console.WriteLine("THIS IS THE CARD GAME OF WAR.  EACH CARD IS GIVEN BY SUIT-#");
-			Console.WriteLine("AS S-7 FOR SPADE 7.  ");
-	
-			if (AskQuestion("DO YOU WANT DIRECTIONS? "))
-			{
-				Console.WriteLine("THE COMPUTER GIVES YOU AND IT A 'CARD'.  THE HIGHER CARD");
-				Console.WriteLine("(NUMERICALLY) WINS.  THE GAME ENDS WHEN YOU CHOOSE NOT TO");
-				Console.WriteLine("CONTINUE OR WHEN YOU HAVE FINISHED THE PACK.");
-			}
-	
-			Console.WriteLine();
-			Console.WriteLine();
-		}
-	
-		public bool AskQuestion(string question)
-		{
-			while (true)
-			{
-				Console.Write(question);
-				string result = Console.ReadLine();
-	
-				if (result.ToLower()[0] == 'y')
-				{
-					return true;
-				}
-				else /*if (result.ToLower() == "no")*/
-				{
-					return false;
-				}
-	
-				Console.WriteLine("YES OR NO, PLEASE.");
-			}
-		}
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var intro = new Intro();
-			intro.WriteIntro();
+            var ui = new UserInterface();
+			ui.WriteIntro();
 
 			var deck = new Deck();
 			deck.Shuffle();
@@ -63,38 +16,20 @@ namespace War
 
             for (int i = 0; i < Deck.deckSize; i += 2)
             {
+                // Play the next hand.
 				var yourCard = deck.GetCard(i);
                 var computersCard = deck.GetCard(i + 1);
 
-                Console.WriteLine($"YOU: {yourCard}     COMPUTER: {computersCard}");
-				if (yourCard < computersCard)
-                {
-					computersScore++;
-                    Console.WriteLine($"THE COMPUTER WINS!!! YOU HAVE {yourScore} AND THE COMPUTER HAS {computersScore}");
-                }
-                else if (yourCard > computersCard)
-                {
-					yourScore++;
-                    Console.WriteLine($"YOU WIN. YOU HAVE {yourScore} AND THE COMPUTER HAS {computersScore}");
-                }
-				else
-                {
-                    Console.WriteLine("TIE.  NO SCORE CHANGE");
-                }
+                ui.WriteAResult(yourCard, computersCard, ref computersScore, ref yourScore);
 
-				if (!intro.AskQuestion("DO YOU WANT TO CONTINUE? "))
+                if (!ui.AskAQuestion("DO YOU WANT TO CONTINUE? "))
                 {
 					usedAllCards = false;
 					break;
                 }
             }
 
-			if (usedAllCards)
-            {
-				Console.WriteLine("WE HAVE RUN OUT OF CARDS.");
-            }
-			Console.WriteLine($"FINAL SCORE:  YOU: {yourScore}  THE COMPUTER: {computersScore}");
-			Console.WriteLine("THANKS FOR PLAYING.  IT WAS FUN.");
+            ui.WriteClosingRemarks(usedAllCards, yourScore, computersScore);
         }
     }
 }
