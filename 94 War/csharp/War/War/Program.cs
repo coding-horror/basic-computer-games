191b77f54c302024bc8c63163ebafe3fe6f9746a
rbamforth <79797573+rbamforth@users.noreply.github.com>
2021-03-26 23:03:15 +0000

    Fixed tabs, spaces mismatch.

--- 94 War/csharp/War/War/Program.cs
+++ 94 War/csharp/War/War/Program.cs
@@ -5,27 +5,27 @@
         static void Main(string[] args)
         {
             var ui = new UserInterface();
-			ui.WriteIntro();
+            ui.WriteIntro();
 
-			var deck = new Deck();
-			deck.Shuffle();
+            var deck = new Deck();
+            deck.Shuffle();
 
-			int yourScore = 0;
+            int yourScore = 0;
             int computersScore = 0;
-			bool usedAllCards = true;
+            bool usedAllCards = true;
 
             for (int i = 0; i < Deck.deckSize; i += 2)
             {
                 // Play the next hand.
-				var yourCard = deck.GetCard(i);
+                var yourCard = deck.GetCard(i);
                 var computersCard = deck.GetCard(i + 1);
 
                 ui.WriteAResult(yourCard, computersCard, ref computersScore, ref yourScore);
 
                 if (!ui.AskAQuestion("DO YOU WANT TO CONTINUE? "))
                 {
-					usedAllCards = false;
-					break;
+                    usedAllCards = false;
+                    break;
                 }
             }
 
