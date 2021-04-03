74f758fd35eb27e4a0817531d7684448ca00920b
Peter <peterruderman@hotmail.com>
2021-04-03 02:21:06 -0400

    Ported Hammurabi to C#

---
+++ 43 Hammurabi/csharp/src/ActionResult.cs
@@ -0,0 +1,37 @@
+﻿namespace Hammurabi
+{
+    /// <summary>
+    /// Enumerates the different possible outcomes of attempting the various
+    /// actions in the game.
+    /// </summary>
+    public enum ActionResult
+    {
+        /// <summary>
+        /// The action was a success.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// The action could not be completed because the city does not have
+        /// enough bushels of grain.
+        /// </summary>
+        InsufficientStores,
+
+        /// <summary>
+        /// The action could not be completed because the city does not have
+        /// sufficient acreage.
+        /// </summary>
+        InsufficientLand,
+
+        /// <summary>
+        /// The action could not be completed because the city does not have
+        /// sufficient population.
+        /// </summary>
+        InsufficientPopulation,
+
+        /// <summary>
+        /// The requested action offended the city steward.
+        /// </summary>
+        Offense
+    }
+}
---
+++ 43 Hammurabi/csharp/src/Controller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Hammurabi
+{
+    /// <summary>
+    /// Provides methods for reading input from the user.
+    /// </summary>
+    public static class Controller
+    {
+        /// <summary>
+        /// Continuously prompts the user to enter a number until he or she
+        /// enters a valid number and updates the game state.
+        /// </summary>
+        /// <param name="state">
+        /// The current game state.
+        /// </param>
+        /// <param name="prompt">
+        /// Action that will display the prompt to the user.
+        /// </param>
+        /// <param name="rule">
+        /// The rule to invoke once input is retrieved.
+        /// </param>
+        /// <returns>
+        /// The updated game state.
+        /// </returns>
+        public static GameState TryUntilSuccess(
+            GameState state,
+            Action prompt,
+            Func<GameState, int, (GameState newState, ActionResult result)> rule)
+        {
+            while (true)
+            {
+                prompt();
+
+                if (!Int32.TryParse(Console.ReadLine(), out var amount))
+                {
+                    View.ShowInvalidNumber();
+                }
+                else
+                {
+                    var (newState, result) = rule(state, amount);
+
+                    switch (result)
+                    {
+                        case ActionResult.InsufficientLand:
+                            View.ShowInsufficientLand(state);
+                            break;
+                        case ActionResult.InsufficientPopulation:
+                            View.ShowInsufficientPopulation(state);
+                            break;
+                        case ActionResult.InsufficientStores:
+                            View.ShowInsufficientStores(state);
+                            break;
+                        case ActionResult.Offense:
+                            // Not sure why we have to blow up the game here...
+                            // Maybe this made sense in the 70's.
+                            throw new GreatOffence();
+                        default:
+                            return newState;
+                    }
+                }
+            }
+        }
+    }
+}
