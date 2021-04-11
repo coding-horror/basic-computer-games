b77de83253d68c049d760670729c6046ab9d61c6
rbamforth <79797573+rbamforth@users.noreply.github.com>
2021-04-11 20:57:43 +0100

    Added test that dice rolls are random.

--- 29 Craps/csharp/Craps/Craps/Program.cs
+++ 29 Craps/csharp/Craps/Craps/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
+
+
 namespace Craps
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
