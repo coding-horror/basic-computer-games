af4013c47f9576e87d4f8af6e6c31f7b14a8a9a3
Andrew Cooper <andrew.cooper@nearmap.com>
2021-04-17 18:49:34 +1000

    Add C# project

---
+++ 82 Stars/csharp/Stars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
