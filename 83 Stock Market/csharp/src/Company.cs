544dd3968d01d85d116a30de1932ba87f391158c
Peter <peterruderman@hotmail.com>
2021-06-15 17:37:40 -0400

    Ported Stock Market to C#

---
+++ 83 Stock Market/csharp/src/Company.cs
@@ -0,0 +1,23 @@
+﻿namespace Game
+{
+    /// <summary>
+    /// Represents a company.
+    /// </summary>
+    public record Company
+    {
+        /// <summary>
+        /// Gets the company's name.
+        /// </summary>
+        public string Name { get; init; }
+
+        /// <summary>
+        /// Gets the company's three letter stock symbol.
+        /// </summary>
+        public string StockSymbol { get; init; }
+
+        /// <summary>
+        /// Gets the company's current share price.
+        /// </summary>
+        public double SharePrice { get; init; }
+    }
+}
