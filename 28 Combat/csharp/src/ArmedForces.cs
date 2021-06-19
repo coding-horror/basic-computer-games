dc2375e4af3101e8ad473b4a75b037c65a51ec75
Peter <peterruderman@hotmail.com>
2021-06-19 13:27:38 -0400

    Ported Combat to C#

---
+++ 28 Combat/csharp/src/ArmedForces.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Game
+{
+    /// <summary>
+    /// Represents the armed forces for a country.
+    /// </summary>
+    public record ArmedForces
+    {
+        /// <summary>
+        /// Gets the number of men and women in the army.
+        /// </summary>
+        public int Army { get; init; }
+
+        /// <summary>
+        /// Gets the number of men and women in the navy.
+        /// </summary>
+        public int Navy { get; init; }
+
+        /// <summary>
+        /// Gets the number of men and women in the air force.
+        /// </summary>
+        public int AirForce { get; init; }
+
+        /// <summary>
+        /// Gets the total number of troops in the armed forces.
+        /// </summary>
+        public int TotalTroops => Army + Navy + AirForce;
+
+        /// <summary>
+        /// Gets the number of men and women in the given branch.
+        /// </summary>
+        public int this[MilitaryBranch branch] =>
+            branch switch
+            {
+                MilitaryBranch.Army     => Army,
+                MilitaryBranch.Navy     => Navy,
+                MilitaryBranch.AirForce => AirForce,
+                _                       => throw new ArgumentException("INVALID BRANCH")
+            };
+    }
+}
