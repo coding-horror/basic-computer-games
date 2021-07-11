d8a693e38684357c1c097f3e2919c72ebc188d5e
Peter <peterruderman@hotmail.com>
2021-07-11 09:14:38 -0400

    Ported Mastermind to C#

---
+++ 60 Mastermind/csharp/Game/src/EnumerableExtensions.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game
+{
+    /// <summary>
+    /// Provides additional methods for the <see cref="IEnumerable{T}"/>
+    /// interface.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Cycles through the integer values in the range [0, count).
+        /// </summary>
+        /// <param name="start">
+        /// The first value to return.
+        /// </param>
+        /// <param name="count">
+        /// The number of values to return.
+        /// </param>
+        public static IEnumerable<int> Cycle(int start, int count)
+        {
+            if (count < 1)
+                throw new ArgumentException("count must be at least 1");
+
+            if (start < 0 || start >= count)
+                throw new ArgumentException("start must be in the range [0, count)");
+
+            for (var i = start; i < count; ++i)
+                yield return i;
+
+            for (var i = 0; i < start; ++i)
+                yield return i;
+        }
+
+        /// <summary>
+        /// Finds the index of the first item in the given sequence that
+        /// satisfies the given predicate.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of elements in the sequence.
+        /// </typeparam>
+        /// <param name="source">
+        /// The source sequence.
+        /// </param>
+        /// <param name="predicate">
+        /// The predicate function.
+        /// </param>
+        /// <returns>
+        /// The index of the first element in the source sequence for which
+        /// predicate(element) is true.  If there is no such element, return
+        /// is null.
+        /// </returns>
+        public static int? FindFirstIndex<T>(this IEnumerable<T> source, Func<T, bool> predicate) =>
+            source.Select((element, index) => predicate(element) ? index : default(int?))
+                .FirstOrDefault(index => index.HasValue);
+
+        /// <summary>
+        /// Returns the first item in the given sequence that matches the
+        /// given predicate.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of elements in the sequence.
+        /// </typeparam>
+        /// <param name="source">
+        /// The source sequence.
+        /// </param>
+        /// <param name="predicate">
+        /// The predicate to check against each element.
+        /// </param>
+        /// <param name="defaultValue">
+        /// The value to return if no elements match the predicate.
+        /// </param>
+        /// <returns>
+        /// The first item in the source sequence that matches the given
+        /// predicate, or the provided default value if none do.
+        /// </returns>
+        public static T FirstOrDefault<T>(this IEnumerable<T> source, Func<T, bool> predicate, T defaultValue)
+        {
+            foreach (var element in source)
+                if (predicate(element))
+                    return element;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Maps the values of the source sequence to a nullable type and
+        /// returns the non-null values.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of elements in the source sequence.
+        /// </typeparam>
+        /// <param name="source">
+        /// The source sequence.
+        /// </param>
+        /// <param name="selector">
+        /// The selector function.
+        /// </param>
+        public static IEnumerable<TResult> SelectNonNull<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult?> selector) where TResult: struct
+        {
+            foreach (var element in source)
+            {
+                var result = selector(element);
+                if (result.HasValue)
+                    yield return result.Value;
+            }
+        }
+    }
+}
---
+++ 60 Mastermind/csharp/Game/src/View.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game
+{
+    /// <summary>
+    /// Contains functions for displaying information to the end user.
+    /// </summary>
+    public static class View
+    {
+        public static void ShowBanner()
+        {
+            Console.WriteLine("                              MASTERMIND");
+            Console.WriteLine("               CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static void ShowStartOfRound(int roundNumber)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"ROUND NUMBER {roundNumber} ----");
+            Console.WriteLine();
+            Console.WriteLine("GUESS MY COMBINATION.");
+            Console.WriteLine();
+        }
+
+        public static void ShowTotalPossibilities(int possibilities)
+        {
+            Console.WriteLine($"TOTAL POSSIBILITIES = {possibilities}");
+            Console.WriteLine();
+        }
+
+        public static void ShowColorTable(int numberOfColors)
+        {
+            Console.WriteLine();
+            Console.WriteLine("COLOR     LETTER");
+            Console.WriteLine("=====     ======");
+
+            foreach (var color in Colors.List.Take(numberOfColors))
+                Console.WriteLine($"{color.LongName,-13}{color.ShortName}");
+
+            Console.WriteLine();
+        }
+
+        public static void ShowBoard(IEnumerable<TurnResult> history)
+        {
+            Console.WriteLine();
+            Console.WriteLine("BOARD");
+            Console.WriteLine("MOVE     GUESS          BLACK     WHITE");
+
+            var moveNumber = 0;
+            foreach (var result in history)
+                Console.WriteLine($"{++moveNumber,-9}{result.Guess,-16}{result.Blacks,-10}{result.Whites}");
+
+            Console.WriteLine();
+        }
+
+        public static void ShowQuitGame(Code code)
+        {
+            Console.WriteLine($"QUITTER!  MY COMBINATION WAS: {code}");
+            Console.WriteLine("GOOD BYE");
+        }
+
+        public static void ShowResults(int blacks, int whites)
+        {
+            Console.WriteLine($"YOU HAVE  {blacks}  BLACKS AND  {whites}  WHITES.");
+        }
+
+        public static void ShowHumanGuessedCode(int guessNumber)
+        {
+            Console.WriteLine($"YOU GUESSED IT IN  {guessNumber}  MOVES!");
+        }
+
+        public static void ShowHumanFailedToGuessCode(Code code)
+        {
+            // Note: The original code did not print out the combination, but
+            // this appears to be a bug.
+            Console.WriteLine("YOU RAN OUT OF MOVES!  THAT'S ALL YOU GET!");
+            Console.WriteLine($"THE ACTUAL COMBINATION WAS: {code}");
+        }
+
+        public static void ShowScores(int humanScore, int computerScore, bool isFinal)
+        {
+            if (isFinal)
+            {
+                Console.WriteLine("GAME OVER");
+                Console.WriteLine("FINAL SCORE:");
+            }
+            else
+                Console.WriteLine("SCORE:");
+
+            Console.WriteLine($"     COMPUTER  {computerScore}");
+            Console.WriteLine($"     HUMAN     {humanScore}");
+            Console.WriteLine();
+        }
+
+        public static void ShowComputerStartTurn()
+        {
+            Console.WriteLine("NOW I GUESS.  THINK OF A COMBINATION.");
+        }
+
+        public static void ShowInconsistentInformation()
+        {
+            Console.WriteLine("YOU HAVE GIVEN ME INCONSISTENT INFORMATION.");
+            Console.WriteLine("TRY AGAIN, AND THIS TIME PLEASE BE MORE CAREFUL.");
+        }
+
+        public static void ShowComputerGuessedCode(int guessNumber)
+        {
+            Console.WriteLine($"I GOT IT IN  {guessNumber}  MOVES!");
+        }
+
+        public static void ShowComputerFailedToGuessCode()
+        {
+            Console.WriteLine("I USED UP ALL MY MOVES!");
+            Console.WriteLine("I GUESS MY CPU IS JUST HAVING AN OFF DAY.");
+        }
+
+        public static void PromptNumberOfColors()
+        {
+            Console.Write("NUMBER OF COLORS? ");
+        }
+
+        public static void PromptNumberOfPositions()
+        {
+            Console.Write("NUMBER OF POSITIONS? ");
+        }
+
+        public static void PromptNumberOfRounds()
+        {
+            Console.Write("NUMBER OF ROUNDS? ");
+        }
+
+        public static void PromptGuess(int moveNumber)
+        {
+            Console.Write($"MOVE #  {moveNumber}  GUESS ? ");
+        }
+
+        public static void PromptReady()
+        {
+            Console.Write("HIT RETURN WHEN READY ? ");
+        }
+
+        public static void PromptBlacksWhites(Code code)
+        {
+            Console.Write($"MY GUESS IS: {code}");
+            Console.Write("  BLACKS, WHITES ? ");
+        }
+
+        public static void PromptTwoValues()
+        {
+            Console.WriteLine("PLEASE ENTER TWO VALUES, SEPARATED BY A COMMA");
+        }
+
+        public static void PromptValidInteger()
+        {
+            Console.WriteLine("PLEASE ENTER AN INTEGER VALUE");
+        }
+
+        public static void NotifyBadNumberOfPositions()
+        {
+            Console.WriteLine("BAD NUMBER OF POSITIONS");
+        }
+
+        public static void NotifyInvalidColor(char colorKey)
+        {
+            Console.WriteLine($"'{colorKey}' IS UNRECOGNIZED.");
+        }
+
+        public static void NotifyTooManyColors(int maxColors)
+        {
+            Console.WriteLine($"NO MORE THAN {maxColors}, PLEASE!");
+        }
+    }
+}
