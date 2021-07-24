4c6caa2148fc1305e6701e3dc7bbea7e6cba7fe5
Peter <peterruderman@hotmail.com>
2021-07-23 20:02:54 -0400

    Reworked Bull Fight in a reactive style

---
+++ 17 Bullfight/csharp/src/Events/PlayerGored.cs
@@ -0,0 +1,7 @@
+﻿namespace Game.Events
+{
+    /// <summary>
+    /// Indicates that the player has been gored by the bull.
+    /// </summary>
+    public sealed record PlayerGored(bool Panicked, bool FirstGoring) : Event;
+}
