82f00eac971292019705907b774fff20819e1d59
Andrew Cooper <andrew.cooper@nearmap.com>
2021-10-04 17:45:55 +1100

    Add C# implementaion of 86 Target

---
+++ 86 Target/csharp/Target/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace Target
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayTitleAndInstructions();
+
+            var firingRange = new FiringRange();
+
+            while (true)
+            {
+                Game.Play(firingRange);
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Next target...");
+                Console.WriteLine();
+
+                firingRange.NextTarget();
+            }
+        }
+
+        private static void DisplayTitleAndInstructions()
+        {
+            using var stream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("Target.Strings.TitleAndInstructions.txt");
+            using var stdout = Console.OpenStandardOutput();
+
+            stream.CopyTo(stdout);
+        }
+    }
+}
