71ed6e3f95102f074112f6bfdd5b93378882f11d
Andrew Cooper <andrew.cooper@nearmap.com>
2021-10-11 23:27:58 +1100

    Implement 58 Love in C#

---
+++ 58 Love/csharp/Love/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace Love
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayIntro();
+
+            var message = Input.ReadLine("Your message, please");
+            var pattern = new LovePattern();
+
+            var source = new SourceCharacters(pattern.LineLength, message);
+
+            using var destination = Console.OpenStandardOutput();
+
+            pattern.Write(source, destination);
+        }
+
+        private static void DisplayIntro()
+        {
+            using var stream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("Love.Strings.Intro.txt");
+            using var stdout = Console.OpenStandardOutput();
+
+            stream.CopyTo(stdout);
+        }
+    }
+}
