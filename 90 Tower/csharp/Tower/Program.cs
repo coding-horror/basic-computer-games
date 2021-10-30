0321f3c6c6cf186a8fbc2a0bcd4d598125d43b08
Andrew Cooper <andrew.cooper@nearmap.com>
2021-10-30 22:32:22 +1100

    Add project sturcture

---
+++ 90 Tower/csharp/Tower/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
