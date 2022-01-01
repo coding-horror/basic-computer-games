2a6dc209c8efc96ae84e31f45f89abecf0b74313
Piotr Czajkowski <pczajkowski@gmail.com>
2022-01-01 11:17:05 +0100

    Added Instructions

--- 20_Buzzword/csharp/Program.cs
+++ 20_Buzzword/csharp/Program.cs
@@ -12,9 +12,22 @@ namespace Buzzword
             Console.WriteLine();
             Console.WriteLine();
         }
+
+        static void Instructions()
+        {
+            Console.WriteLine("This program prints highly acceptable phrases in\n"
+            + "'Educator-speak'that you can work into reports\n"
+            + "and speeches. Whenever a question mark is printed,\n"
+            + "type a 'Y' for another phrase or 'N' to quit.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Here's the first phrase:");
+        }
+
         static void Main(string[] args)
         {
             Header();
+            Instructions();
         }
     }
 }
