febedb72bb45ca84ad2be834d6c6b878503112ed
Piotr Czajkowski <pczajkowski@gmail.com>
2022-01-01 11:12:12 +0100

    Added header

---
+++ 20_Buzzword/csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Buzzword
+{
+    class Program
+    {
+        static void Header()
+        {
+            Console.WriteLine("Buzzword generator".PadLeft(26));
+            Console.WriteLine("Creating Computing Morristown, New Jersey".PadLeft(15));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Header();
+        }
+    }
+}
