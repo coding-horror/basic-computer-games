4ca8ea1e2324bf564e0ba1e78413bffdf73fa0f0
Piotr Czajkowski <pczajkowski@gmail.com>
2022-01-02 13:04:39 +0100

    Added Header

---
+++ 22_Change/csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Change
+{
+    class Program
+    {
+        static void Header()
+        {
+            Console.WriteLine("Change".PadLeft(33));
+            Console.WriteLine("Creative Computing Morristown, New Jersey".PadLeft(15));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("I, your friendly microcomputer, will determine\n"
+            + "the correct change for items costing up to $100.");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Header();
+        }
+    }
+}
