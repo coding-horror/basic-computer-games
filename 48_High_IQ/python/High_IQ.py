24313121ca9d0d61b038776f8ee94e74f9497f62
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-04 12:00:29 -0500

    Started python function layout
    Totally not doing this through github.com

--- 48_High_IQ/python/High_IQ.py
+++ 48_High_IQ/python/High_IQ.py
@@ -1,1 +1,13 @@
 
+def print_instructions():
+    print("This is where you will find instructions")
+    
+def play_game():
+    print("Lets play a game")
+  
+def main():
+    if input("Do you want instrunctions?").lower().startswith("y"):
+        print_instructions()
+    
+if __name__ == "__main__":
+    main()
