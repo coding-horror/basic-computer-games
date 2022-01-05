62d54183c7c67d6d133bf0e8f1409356e54d86ac
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2022-01-05 16:45:12 -0300

    Formatting comments

--- 85_Synonym/ruby/synonim.rb
+++ 85_Synonym/ruby/synonim.rb
@@ -4,19 +4,19 @@
 #
 # From Basic Computer Games (1978)
 #
-#A synonym of a word is another word (in the English language) which has the same,
-#or very nearly the same, meaning. This program tests your knowledge of synonyms
-#of a few common words.
+# A synonym of a word is another word (in the English language) which has the same,
+# or very nearly the same, meaning. This program tests your knowledge of synonyms
+# of a few common words.
 #
-#The computer chooses a word and asks you for a synonym. The computer then tells
-#you whether you’re right or wrong. If you can’t think of a synonym, type “HELP”
-#which causes a synonym to be printed.
-#You may put in words of your choice in the data statements.
-#The number following DATA in Statement 500 is the total number of data statements.
-#In each data statement, the first number is the number of words in that statement.
+# The computer chooses a word and asks you for a synonym. The computer then tells
+# you whether you’re right or wrong. If you can’t think of a synonym, type “HELP”
+# which causes a synonym to be printed.
+# You may put in words of your choice in the data statements.
+# The number following DATA in Statement 500 is the total number of data statements.
+# In each data statement, the first number is the number of words in that statement.
 #
-#Can you think of a way to make this into a more general kind of CAI program for any subject?
-#Walt Koetke of Lexington High School, Massachusetts created this program.
+# Can you think of a way to make this into a more general kind of CAI program for any subject?
+# Walt Koetke of Lexington High School, Massachusetts created this program.
 #
 #
 ########################################################
