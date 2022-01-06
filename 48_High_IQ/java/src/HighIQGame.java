048b9c31a283b6b40bafd3480cd7cc5667f1eab6
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-06 11:38:17 -0500

    Added re-playability

--- 48_High_IQ/java/src/HighIQGame.java
+++ 48_High_IQ/java/src/HighIQGame.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class HighIQGame {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        do {
+            new HighIQ(scanner).play();
+            System.out.println("PLAY AGAIN (YES OR NO)");
+        } while(scanner.nextLine().toLowerCase().equals("yes"));
     }
 }
