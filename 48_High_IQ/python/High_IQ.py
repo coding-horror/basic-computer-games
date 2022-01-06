2811e23a4f23e69a52790067790103f96ab4fcbf
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-06 08:36:05 -0500

    Finished print_board(board) method

--- 48_High_IQ/python/High_IQ.py
+++ 48_High_IQ/python/High_IQ.py
@@ -42,7 +42,9 @@ def print_board(board):
     print(board[29] + board[30] + board[31] + board[32] + board[33] + board[34] + board[35])
     print(board[38] + board[39] + board[40] + board[41] + board[42] + board[43] + board[44])
     print(board[47] + board[48] + board[49] + board[50] + board[51] + board[52] + board[53])
-
+    print(" " * 3 + board[58] + board[59] + board[60] + " " * 3)
+    print(" " * 3 + board[67] + board[68] + board[69] + " " * 3)
+    
 def play_game():
     print("Lets play a game")
     board = new_board()
