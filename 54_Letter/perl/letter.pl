5b0d5e1221490818c433e4a19d97f450f2a61e58
Pat Ludwig <havoc@boldo.com>
2022-01-06 19:49:26 -0600

    Fix misspelling
    General code cleanup, including
    * Address code indendation
    * Condense print statments
    * Add spaces before = signs
    * Add spaces around operators
    * Simplify a few things
    * Use full variable names instead of single letters

--- 54_Letter/perl/letter.pl
+++ 54_Letter/perl/letter.pl
@@ -5,40 +5,37 @@ print ' 'x33 . "LETTER\n";
 print ' 'x15 . "CREATIVE COMPUTING MORRISTOWN, NEW JERSEY\n";
 print "\n\n\n";
 
-print "LETTER GUESSING GAME\n"; print "\n";
+print "LETTER GUESSING GAME\n\n";
 print "I'LL THINK OF A LETTER OF THE ALPHABET, A TO Z.\n";
 print "TRY TO GUESS MY LETTER AND I'LL GIVE YOU CLUES\n";
 print "AS TO HOW CLOSE YOU'RE GETTING TO MY LETTER.\n";
 
-my $A;
 while (1) {
-	my $L= 65+int(rand(1)*26);
-	my $G= 0;
-	print "\n"; print "O.K., I HAVE A LETTER. START GUESSING.\n";
+	my $letter = 65 + int(rand(26));
+	my $guesses = 0;
+	print "\nO.K., I HAVE A LETTER. START GUESSING.\n";
 
+	my $answer;
 	do {
-		print "\n"; print "WHAT IS YOUR GUESS? ";
-		$G=$G+1;
-		chomp($A= <STDIN>);
-		$A= ord($A);
+		print "\nWHAT IS YOUR GUESS? ";
+		$guesses++;
+		chomp($answer = <STDIN>);
+		$answer = ord($answer);
 		print "\n";
-		if ($A<$L) { print "TOO LOW. TRY A HIGHER LETTER.\n"; }
-		if ($A>$L) { print "TOO HIGH. TRY A LOWER LETTER.\n"; }
-		} until($A eq $L);
+		print "TOO LOW. TRY A HIGHER LETTER.\n" if $answer < $letter;
+		print "TOO HIGH. TRY A LOWER LETTER.\n" if $answer > $letter;
+	} until($answer eq $letter);
 
-	print "\n"; print "YOU GOT IT IN $G GUESSES!!\n";
+	print "\nYOU GOT IT IN $guesses GUESSES!!\n";
 
-	if ($G<=5) {
+	if ($guesses <= 5) {
 		print "GOOD JOB !!!!!\n";
-		for (my $N=1; $N<=15; $N++) { print chr(7); } #ASCII Bell.
-		} else {
+		print chr(7) x 15; # ASCII Bell
+	} else {
 		print "BUT IT SHOULDN'T TAKE MORE THAN 5 GUESSES!\n";
-		}
-
-	print "\n";
-	print "LET'S PLAN AGAIN.....\n";
 	}
 
+	print "\nLET'S PLAY AGAIN.....\n";
+}
+
 exit;
-
-
