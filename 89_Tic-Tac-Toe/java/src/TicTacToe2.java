cafa60a2650ebca044d9d0366a78addaf8bea807
Paul Holt <pcholt@gmail.com>
2022-01-08 01:57:12 +1100

    oops revert renaming TicTacToe2

--- 89_Tic-Tac-Toe/java/src/TicTacToe2.java
+++ 89_Tic-Tac-Toe/java/src/TicTacToe2.java
@@ -4,7 +4,7 @@ import java.util.Random;
 /**
  * @author Ollie Hensman-Crook
  */
-public class TicTacToe {
+public class TicTacToe2 {
     public static void main(String[] args) {
         Board gameBoard = new Board();
         Random compChoice = new Random();
