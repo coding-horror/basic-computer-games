ea5c2cf72d3164654894431acb741ec971ea8a3b
andrew <andrew@arobeia.co.uk>
2022-01-10 20:26:50 +0000

    Comment the input class

--- 09_Battle/java/Input.java
+++ 09_Battle/java/Input.java
@@ -3,12 +3,15 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.text.NumberFormat;
 
+// This class handles reading input from the player
+// Each input is an x and y coordinate
+// e.g. 5,3
 public class Input {
     private BufferedReader reader;
     private NumberFormat parser;
-    private int scale;
-    private boolean isQuit;
-    private int[] coords;
+    private int scale;             // size of the sea, needed to validate input
+    private boolean isQuit;        // whether the input has ended
+    private int[] coords;          // the last coordinates read
 
     public Input(int seaSize) {
         scale = seaSize;
@@ -18,22 +21,27 @@ public class Input {
 
     public boolean readCoordinates() throws IOException {
         while (true) {
+            // Write a prompt
             System.out.print("\nTarget x,y\n> ");
             String inputLine = reader.readLine();
             if (inputLine == null) {
-                System.out.println("Game quit\n");
+                // If the input stream is ended, there is no way to continue the game
+                System.out.println("\nGame quit\n");
                 isQuit = true;
                 return false;
             }
 
+            // split the input into two fields
             String[] fields = inputLine.split(",");
             if (fields.length != 2) {
+                // has to be exactly two
                 System.out.println("Need two coordinates separated by ','");
                 continue;
             }
             
             coords = new int[2];
             boolean error = false;
+            // each field should contain an integer from 1 to the size of the sea 
             try {
                 for (int c = 0 ; c < 2; ++c ) {
                     int val = Integer.parseInt(fields[c].strip());
@@ -46,6 +54,7 @@ public class Input {
                 }
             }
             catch (NumberFormatException ne) {
+                // this happens if the field is not a valid number
                 System.out.println("Coordinates must be numbers");
                 error = true;
             }
