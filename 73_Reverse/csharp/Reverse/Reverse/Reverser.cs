84d473d8f29e65b23ce12a859a45479974b5fe0c
Kristian Stolen <kstolen0@our.ecu.edu.au>
2022-01-12 17:56:26 +0800

    Create reverser that reverses an array at a given position.

---
+++ 73_Reverse/csharp/Reverse/Reverse/Reverser.cs
@@ -0,0 +1,16 @@
+﻿namespace Reverse
+{
+    public class Reverser
+    {
+        public static void Reverse(int[] arrayToReverse, int indexToReverseTo)
+        {
+            for (int i = 0; i < indexToReverseTo / 2; i++)
+            {
+                int temp = arrayToReverse[i];
+                int upperIndex = indexToReverseTo - 1 - i;
+                arrayToReverse[i] = arrayToReverse[upperIndex];
+                arrayToReverse[upperIndex] = temp;
+            }
+        }
+    }
+}
