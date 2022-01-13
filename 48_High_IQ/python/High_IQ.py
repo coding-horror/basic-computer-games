3ec94a28880d7e838996afb8ff37523cba91e807
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-13 08:47:41 -0500

    Removed Commented Line

--- 48_High_IQ/python/High_IQ.py
+++ 48_High_IQ/python/High_IQ.py
@@ -84,7 +84,6 @@ def move(board):
         return False
 
 def main():
-#     if input("Do you want instrunctions?\n").lower().startswith("y"):
     print(" " * 33 + "H-I-Q")
     print(" " * 15 + "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY")
     print_instructions()
@@ -94,8 +93,8 @@ def is_game_finished(board):
     for pos in board.keys():
         if board[pos] == "X":
             for space in [1,9]:
-                nextToPeg = ((pos + space) in board) and board[pos + space] == "X"
-                hasMovableSpace = (not ((pos - space) in board and board[pos - space] == "X")) or (not ((pos + space * 2) in board and board[pos + space * 2] == "X"))
+                nextToPeg = ((pos + space) in board) and board[pos + space] == "!"
+                hasMovableSpace = (not ((pos - space) in board and board[pos - space] == "!")) or (not ((pos + space * 2) in board and board[pos + space * 2] == "!"))
                 if nextToPeg and hasMovableSpace:
                     return False
 
