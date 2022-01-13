0a161c717b6129eca94b57044e04905d08699fc4
Zev Spitz <shivisi@hotmail.com>
2022-01-13 16:24:46 +0200

    Some utility scripts for C# and VB.NET ports

---
+++ 00_Utilities/DotnetUtils/DotnetUtils/Globals.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetUtils;
+
+public static class Globals {
+    public static readonly Dictionary<string, (string codefileExtension, string projExtension)> LangData = new() {
+        { "csharp", ("cs", "csproj") },
+        { "vbnet", ("vb", "vbproj") }
+    };
+}
