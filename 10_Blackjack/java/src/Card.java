6991f66012c2b814370e5840031ed68bd1ab84c3
Mitch Peck <mjohnpeck@gmail.com>
2022-01-22 20:15:09 -0600

    Add Player and Card objects

---
+++ 10_Blackjack/java/src/Card.java
@@ -0,0 +1,21 @@
+public class Card {
+    private int value;
+    private String suit;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    } 
+    
+    public void setSuit(int suit) {
+        this.suit = suit;
+    }
+
+    public int getSuit() {
+        return this.suit;
+    }
+
+}
