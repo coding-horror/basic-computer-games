3411d33f182674004052c1f1ba6208615c5290c7
Mitch Peck <mjohnpeck@gmail.com>
2022-01-22 20:15:09 -0600

    Add Player and Card objects

---
+++ 10_Blackjack/java/src/Player.java
@@ -0,0 +1,32 @@
+import Card;
+
+public class Player {
+
+    private int currentBet;
+    private int total;
+    private LinkedList<Card> hand; 
+
+    public void setCurrentBet(int currentBet) {
+        this.currentBet = currentBet;
+    }
+
+    public int getCurrentBet() {
+        return this.currentBet;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getTotal() {
+        return this.total;
+    }
+
+    public void setHand(LinkedList<Card> hand) {
+        this.hand = hand;
+    }
+
+    public LinkedList<Card> getHand() {
+        return this.hand;
+    }
+}
