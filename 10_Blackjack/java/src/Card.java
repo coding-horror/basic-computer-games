de5b3bbf4ca05a5d78cf8fa4fa65f55715a5f93b
Dave Burke <dburke84@gmail.com>
2022-01-23 21:18:32 -0600

    Make Card immutable

--- 10_Blackjack/java/src/Card.java
+++ 10_Blackjack/java/src/Card.java
@@ -1,26 +1,34 @@
-public class Card {
+/**
+ * This is an example of an "immutable" class in Java. That's just a fancy way
+ * of saying the properties (value and suit) can't change after the object has
+ * been created (it has no 'setter' methods and the properties are 'final').
+ *
+ * Immutability often makes it easier to reason about code logic and avoid
+ * certain classes of bugs.
+ *
+ * Since it would never make sense for a card to change in the middle of a game,
+ * this is a good candidate for immutability.
+ *
+ */
+public final class Card {
 
-    public enum Suit {
-        HEARTS, DIAMONDS, SPADES, CLUBS;
-    }
+	public enum Suit {
+		HEARTS, DIAMONDS, SPADES, CLUBS;
+	}
 
-    private int value;
-    private Suit suit;
+    // Since this class is immutable, there's no reason these couldn't be
+    // 'public', but the pattern of using 'getters' is more consistent with
+    // typical Java coding patterns.
+	private final int value;
+	private final Suit suit;
 
-    public void setValue(int value) {
-        this.value = value;
-    }
+	public Card(int value, Suit suit) {
+		this.value = value;
+		this.suit = suit;
+	}
 
-    public int getValue() {
-        return this.value;
-    } 
-    
-    public void setSuit(Suit suit) {
-        this.suit = suit;
-    }
-
-    public Suit getSuit() {
-        return this.suit;
+	public int getValue() {
+		return this.value;
     }
 
 }
