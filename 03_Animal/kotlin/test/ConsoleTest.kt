0ba18afee1c100c342693334356e365acf1ef13d
Paul Holt <pcholt@gmail.com>
2022-01-29 22:36:29 +1100

    Testing added! Now some full behaviour tests on both Animal JVM implementations. Still todo: Move the ConsoleTest library into its own module, add a gradle dependency on it.

---
+++ 03_Animal/kotlin/test/ConsoleTest.kt
@@ -0,0 +1,35 @@
+import com.google.common.truth.Truth
+import org.junit.Rule
+import org.junit.contrib.java.lang.system.SystemOutRule
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream
+
+abstract class ConsoleTest {
+    @get:Rule
+    val inputRule = TextFromStandardInputStream.emptyStandardInputStream()
+
+    @get:Rule
+    val systemOutRule = SystemOutRule().enableLog()
+
+    val regexInputCommand = "\\{(.*)}".toRegex()
+
+    fun assertConversation(conversation: String, runMain: () -> Unit) {
+
+        inputRule.provideLines(*regexInputCommand
+            .findAll(conversation)
+            .map { it.groupValues[1] }
+            .toList().toTypedArray())
+
+        runMain()
+
+        Truth.assertThat(
+            systemOutRule.log.trimWhiteSpace()
+        )
+            .isEqualTo(
+                regexInputCommand
+                    .replace(conversation, "").trimWhiteSpace()
+            )
+    }
+
+    private fun String.trimWhiteSpace() =
+        replace("[\\s]+".toRegex(), " ")
+}
