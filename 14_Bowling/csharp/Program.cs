13a59b7c232a42419bfd65403dda1eddde5a385a
Paul Sobolik <psobolik@zoho.com>
2022-02-06 10:25:54 -0500

    Bowling in csharp

---
+++ 14_Bowling/csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            new Bowling().Play();
+        }
+    }
+}
