279e593a3ed32a7f3aa00d046f2029ebb69b3b8c
Paul Sobolik <psobolik@zoho.com>
2022-02-06 16:30:50 -0500

    Bunny in csharp

---
+++ 19_Bunny/csharp/BasicData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny
+{
+    internal class BasicData
+    {
+        private readonly int[] data;
+
+        private int index;
+
+        public BasicData(int[] data)
+        {
+            this.data = data;
+            index = 0;
+        }
+        public int Read()
+        {
+            return data[index++];
+        }
+    }
+}
