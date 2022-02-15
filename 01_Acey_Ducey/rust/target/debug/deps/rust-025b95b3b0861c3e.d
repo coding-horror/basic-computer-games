9c474f9f51e5a43b9d2d070b1098d3b13ea9d581
mur4ik18 <o.o.kotov1999@gmail.com>
2022-02-15 23:17:17 +0200

    :rocket: Ace game in rust started

---
+++ 01_Acey_Ducey/rust/target/debug/deps/rust-025b95b3b0861c3e.d
@@ -0,0 +1,5 @@
+/home/alex/rust/basic-computer-games-1/01_Acey_Ducey/rust/target/debug/deps/rust-025b95b3b0861c3e.rmeta: src/main.rs
+
+/home/alex/rust/basic-computer-games-1/01_Acey_Ducey/rust/target/debug/deps/rust-025b95b3b0861c3e.d: src/main.rs
+
+src/main.rs:
