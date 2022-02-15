459287a3bf29c24ea6ff470077e00c4ef57b6950
Andrew Cooper <andrew.cooper@nearmap.com>
2022-02-15 22:30:40 +1100

    Add ConsoleIO implementation and sample program

---
+++ 00_Common/dotnet/Games.Common/IO/ConsoleIO.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Games.Common.IO
+{
+    /// <summary>
+    /// An implementation of <see cref="IReadWrite" /> with input begin read for STDIN and output being written to
+    /// STDOUT.
+    /// </summary>
+    public sealed class ConsoleIO : TextIO
+    {
+        public ConsoleIO()
+            : base(Console.In, Console.Out)
+        {
+        }
+    }
+}
