d8e4e0d975b8c3c7e7ae6a20340266b796994d09
Andrew Cooper <andrew.cooper@nearmap.com>
2022-02-15 21:57:21 +1100

    Extract IO string constants

---
+++ 00_Common/dotnet/Games.Common/IO/Strings.cs
@@ -0,0 +1,8 @@
+namespace Games.Common.IO
+{
+    internal static class Strings
+    {
+        internal const string NumberExpected = "!Number expected - retry input line";
+        internal const string ExtraInput = "!Extra input ignored";
+    }
+}
