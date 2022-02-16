55c979c948adf7d2c0fd83baac05bb57d896b563
Alex Kotov <79057640+mur4ik18@users.noreply.github.com>
2022-02-16 05:20:57 +0200

    :wrench: delete trash

--- 01_Acey_Ducey/rust/target/debug/deps/rust-fca9c39bb29b61e9.d
+++
@@ -1,5 +0,0 @@
-/home/alex/rust/basic-computer-games-1/01_Acey_Ducey/rust/target/debug/deps/rust-fca9c39bb29b61e9.rmeta: src/main.rs
-
-/home/alex/rust/basic-computer-games-1/01_Acey_Ducey/rust/target/debug/deps/rust-fca9c39bb29b61e9.d: src/main.rs
-
-src/main.rs:
