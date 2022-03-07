5aaf703bf49390e6aa1d5df73c0dd248eced177e
Andrew Cooper <andrew.cooper@nearmap.com>
2022-03-07 22:03:45 +1100

    Improve Random interface

--- 00_Common/dotnet/Games.Common/Randomness/IRandom.cs
+++ 00_Common/dotnet/Games.Common/Randomness/IRandom.cs
@@ -6,46 +6,16 @@ namespace Games.Common.Randomness;
 public interface IRandom
 {
     /// <summary>
-    /// Gets a random <see cref="float" /> such that 0 <= n < 1.
+    /// Gets a random <see cref="float" /> such that 0 &lt;= n &lt; 1.
     /// </summary>
     /// <returns>The random number.</returns>
     float NextFloat();
 
     /// <summary>
-    /// Gets a random <see cref="float" /> such that 0 <= n < exclusiveMaximum.
-    /// </summary>
-    /// <returns>The random number.</returns>
-    float NextFloat(float exclusiveMaximum);
-
-    /// <summary>
-    /// Gets a random <see cref="float" /> such that inclusiveMinimum <= n < exclusiveMaximum.
-    /// </summary>
-    /// <returns>The random number.</returns>
-    float NextFloat(float inclusiveMinimum, float exclusiveMaximum);
-
-    /// <summary>
-    /// Gets a random <see cref="int" /> such that 0 <= n < exclusiveMaximum.
-    /// </summary>
-    /// <returns>The random number.</returns>
-    int Next(int exclusiveMaximum);
-
-    /// <summary>
-    /// Gets a random <see cref="int" /> such that inclusiveMinimum <= n < exclusiveMaximum.
-    /// </summary>
-    /// <returns>The random number.</returns>
-    int Next(int inclusiveMinimum, int exclusiveMaximum);
-
-    /// <summary>
-    /// Gets the previous random number as a <see cref="float" />.
+    /// Gets the <see cref="float" /> returned by the previous call to <see cref="NextFloat" />.
     /// </summary>
     /// <returns>The previous random number.</returns>
     float PreviousFloat();
-
-    /// <summary>
-    /// Gets the previous random number as an <see cref="int" />.
-    /// </summary>
-    /// <returns>The previous random number.</returns>
-    int Previous();
 
     /// <summary>
     /// Reseeds the random number generator.
--- 00_Common/dotnet/Games.Common/Randomness/RandomNumberGenerator.cs
+++ 00_Common/dotnet/Games.Common/Randomness/RandomNumberGenerator.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace Games.Common.Randomness;
 
+/// <inheritdoc />
 public class RandomNumberGenerator : IRandom
 {
     private Random _random;
@@ -13,38 +14,9 @@ public class RandomNumberGenerator : IRandom
         _random = new Random((int)(DateTime.UtcNow.Ticks / TimeSpan.TicksPerSecond));
     }
 
-    public float NextFloat() => NextFloat(1);
-
-    public float NextFloat(float exclusiveMaximum)
-    {
-        if (exclusiveMaximum <= 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(exclusiveMaximum), "Must be greater than 0.");
-        }
-
-        return NextFloat(0, exclusiveMaximum);
-    }
-
-    public float NextFloat(float inclusiveMinimum, float exclusiveMaximum)
-    {
-        if (exclusiveMaximum <= inclusiveMinimum)
-        {
-            throw new ArgumentOutOfRangeException(nameof(exclusiveMaximum), "Must be greater than inclusiveMinimum.");
-        }
-
-        var range = exclusiveMaximum - inclusiveMinimum;
-        return _previous = ((float)_random.NextDouble()) * range + inclusiveMinimum;
-    }
-
-    public int Next(int exclusiveMaximum) => ToInt(NextFloat(exclusiveMaximum));
-
-    public int Next(int inclusiveMinimum, int exclusiveMaximum) => ToInt(NextFloat(inclusiveMinimum, exclusiveMaximum));
+    public float NextFloat() => _previous = (float)_random.NextDouble();
 
     public float PreviousFloat() => _previous;
-
-    public int Previous() => ToInt(_previous);
-
-    private static int ToInt(float value) => (int)Math.Floor(value);
 
     public void Reseed(int seed) => _random = new Random(seed);
 }
