455fea9609400455ca8baf9e612bfc62c841b54e
Andrew Cooper <andrew.cooper@nearmap.com>
2022-03-13 19:16:41 +1100

    Update 62_Mugwump

--- 62_Mugwump/csharp/Position.cs
+++ 62_Mugwump/csharp/Position.cs
@@ -1,9 +1,8 @@
-namespace Mugwump
-{
-    internal record Position(float X, float Y)
-    {
-        public override string ToString() => $"( {X} , {Y} )";
+namespace Mugwump;
 
-        public static Distance operator -(Position p1, Position p2) => new(p1.X - p2.X, p1.Y - p2.Y);
-    }
+internal record struct Position(float X, float Y)
+{
+    public override string ToString() => $"( {X} , {Y} )";
+
+    public static Distance operator -(Position p1, Position p2) => new(p1.X - p2.X, p1.Y - p2.Y);
 }
