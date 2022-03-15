678aa894396b95b107c7f4a87a0f19a32756f1e3
Auryn Engel <auryn.engel@sap.com>
2022-03-15 13:41:34 +0100

    add acey ducey in elm

---
+++ 01_Acey_Ducey/elm/README.md
@@ -0,0 +1,7 @@
+# Acey Ducey
+
+This is an Elm implementation of the `Basic Compouter Games` Game Acey Ducey.
+
+Find the original repo [here](https://github.com/coding-horror/basic-computer-games).
+
+Live page is [here](https://auryn31.github.io/01_Acey_Ducey/)
---
+++ 01_Acey_Ducey/elm/optimize.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+js="docs/app.js"
+min="docs/app.min.js"
+
+elm make --optimize --output=$js $@
+
+uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
+
+echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
+echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
+echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
