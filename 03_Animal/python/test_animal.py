69e6fdeef0167b3f7ed623fe9b0904ad186ec73f
Martin Thoma <info@martin-thoma.de>
2022-03-19 12:20:57 +0100

    Add Python tests

---
+++ 03_Animal/python/test_animal.py
@@ -0,0 +1,5 @@
+from animal import initial_message
+
+
+def test_initial_message():
+    initial_message()
---
+++ 05_Bagels/python/test_bagels.py
@@ -0,0 +1,5 @@
+from bagels import build_result_string
+
+
+def test_build_result_string():
+    build_result_string(["a", "b", "c"], "abc")
