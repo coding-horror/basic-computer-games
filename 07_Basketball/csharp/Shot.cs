c6d018acef04a8596cdc976d2fb02e86ccd1fb08
Andrew Cooper <andrew.cooper@nearmap.com>
2022-04-12 17:51:18 +1000

    Fix 0 shot for new defense

--- 07_Basketball/csharp/Shot.cs
+++ 07_Basketball/csharp/Shot.cs
@@ -15,12 +15,13 @@ public class Shot
         {
             // Although the game instructions reference two different jump shots,
             // the original game code treats them both the same and just prints "Jump shot"
+            0 => null,
             <= 2 => new JumpShot(),
             3 => new Shot("Lay up"),
             4 => new Shot("Set shot"),
             _ => null
         };
-        return shot is not null;
+        return shotNumber == 0 || shot is not null;
     }
 
     public static Shot Get(float shotNumber) =>
