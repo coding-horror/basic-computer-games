203f930e126a3584f8b5fa98959290299e674795
Andrew Cooper <andrew.cooper@nearmap.com>
2022-07-17 16:48:42 +1000

    Add resources

---
+++ 26_Chomp/csharp/Resources/Resource.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Chomp.Resources;
+
+internal static class Resource
+{
+    internal static class Streams
+    {
+        public static Stream HereWeGo => GetStream();
+        public static Stream Introduction => GetStream();
+        public static Stream Rules => GetStream();
+        public static Stream TooManyColumns => GetStream();
+        public static Stream TooManyRows => GetStream();
+    }
+
+    internal static class Formats
+    {
+        public static string Player => GetString();
+        public static string YouLose => GetString();
+    }
+
+    internal static class Prompts
+    {
+        public static string Coordinates => GetString();
+        public static string HowManyPlayers => GetString();
+        public static string HowManyRows => GetString();
+        public static string HowManyColumns => GetString();
+    }
+
+    private static string GetString([CallerMemberName] string? name = null)
+    {
+        using var stream = GetStream(name);
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+
+
+    private static Stream GetStream([CallerMemberName] string? name = null) =>
+        Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(Resource).Namespace}.{name}.txt")
+            ?? throw new Exception($"Could not find embedded resource stream '{name}'.");
+}
