d1cf340e9fe626e67ad939e9d8ddb95abc42d285
Andrew Cooper <andrew.cooper@nearmap.com>
2022-07-23 18:42:03 +1000

    Add Program and Game class

---
+++ 30_Cube/csharp/Game.cs
@@ -0,0 +1,18 @@
+namespace Cube;
+
+internal class Game
+{
+    private readonly IReadWrite _io;
+    private readonly IRandom _random;
+
+    public Game(IReadWrite io, IRandom random)
+    {
+        _io = io;
+        _random = random;
+    }
+
+    public void Play()
+    {
+        _io.Write(Streams.Introduction);
+    }
+}
---
+++ 30_Cube/csharp/Program.cs
@@ -0,0 +1,8 @@
+global using Games.Common.IO;
+global using Games.Common.Randomness;
+
+global using static Cube.Resources.Resource;
+
+using Cube;
+
+new Game(new ConsoleIO(), new RandomNumberGenerator()).Play();
