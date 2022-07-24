e6d03771069fa0fb55241ca5cca4ececca226782
Andrew Cooper <andrew.cooper@nearmap.com>
2022-07-24 17:29:26 +1000

    Add game loops and betting

--- 30_Cube/csharp/Game.cs
+++ 30_Cube/csharp/Game.cs
@@ -2,6 +2,7 @@ namespace Cube;
 
 internal class Game
 {
+    private const int _initialBalance = 500;
     private readonly IReadWrite _io;
     private readonly IRandom _random;
 
@@ -14,5 +15,42 @@ internal class Game
     public void Play()
     {
         _io.Write(Streams.Introduction);
+
+        if (_io.ReadNumber("") != 0)
+        {
+            _io.Write(Streams.Instructions);
+        }
+
+        PlaySeries(_initialBalance);
+
+        _io.Write(Streams.Goodbye);
+    }
+
+    private void PlaySeries(float balance)
+    {
+        while (true)
+        {
+            var wager = _io.ReadWager(balance);
+
+            var gameWon = PlayGame();
+
+            if (wager.HasValue)
+            {
+                balance = gameWon ? (balance + wager.Value) : (balance - wager.Value);
+                if (balance <= 0)
+                {
+                    _io.Write(Streams.Bust);
+                    return;
+                }
+                _io.WriteLine(Formats.Balance, balance);
+            }
+
+            if (_io.ReadNumber(Prompts.TryAgain) != 1) { return; }
+        }
+    }
+
+    private bool PlayGame()
+    {
+        return true;
     }
 }
