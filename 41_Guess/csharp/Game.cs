9118236a6fee35304977de7e3a1ec096d050f86c
Andrew Cooper <andrew.cooper@nearmap.com>
2022-08-19 19:15:52 +1000

    Add main program classes

---
+++ 41_Guess/csharp/Game.cs
@@ -0,0 +1,18 @@
+namespace Guess;
+
+internal class Game
+{
+    private readonly IReadWrite _io;
+    private readonly IRandom _random;
+
+    public Game(IReadWrite io, IRandom random)
+    {
+        _io = io;
+        _random = random;
+    }
+
+    public void Play()
+    {
+        
+    }
+}
---
+++ 41_Guess/csharp/Program.cs
@@ -0,0 +1,7 @@
+global using Games.Common.IO;
+global using Games.Common.Randomness;
+global using static Guess.Resources.Resource;  
+
+using Guess;
+
+new Game(new ConsoleIO(), new RandomNumberGenerator()).Play();
