276f7f543e9e8dfce5be3a75dedee2b93648574b
Andrew Cooper <andrew.cooper@nearmap.com>
2022-08-20 12:45:43 +1000

    Add program strucutre

---
+++ 56_Life_for_Two/csharp/Program.cs
@@ -0,0 +1,1 @@
+global using Games.Common.IO;
---
+++ 56_Life_for_Two/csharp/Resources/Resource.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace LifeForTwo.Resources;
+
+internal static class Resource
+{
+    internal static class Streams
+    {
+        public static Stream Title => GetStream();
+    }
+
+    internal static class Formats
+    {
+    }
+
+    private static string GetString([CallerMemberName] string? name = null)
+    {
+        using var stream = GetStream(name);
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+
+    private static Stream GetStream([CallerMemberName] string? name = null) =>
+        Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(Resource).Namespace}.{name}.txt")
+            ?? throw new Exception($"Could not find embedded resource stream '{name}'.");
+}
