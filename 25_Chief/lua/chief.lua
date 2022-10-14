6676cd90abf23692412ef5668a9c673dea4ade68
aconconi <aconconi@users.noreply.github.com>
2022-10-14 20:42:02 +0200

    Added credits header to source code.

--- 25_Chief/lua/chief.lua
+++ 25_Chief/lua/chief.lua
@@ -1,3 +1,23 @@
+--[[
+Chief
+
+From: BASIC Computer Games (1978)
+Edited by David H. Ahl
+
+    In the words of the program author, John Graham, “CHIEF is designed to
+    give people (mostly kids) practice in the four operations (addition,
+    multiplication, subtraction, and division).
+
+    It does this while giving people some fun. And then, if the people are
+    wrong, it shows them how they should have done it.
+
+    CHIEF was written by John Graham of Upper Brookville, New York.
+
+    
+Lua port by Alex Conconi, 2022.
+]]--
+
+
 --- Helper function for tabulating messages.
 local function tab(n) return string.rep(" ", n) end
 
