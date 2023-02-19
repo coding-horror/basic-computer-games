e6425221bf3f63342d8f087de27c9190e8427906
drewjcooper <drew.j.cooper@gmail.com>
2023-02-19 19:58:24 +1100

    Create program structure

---
+++ 77_Salvo/csharp/Game.cs
@@ -0,0 +1,18 @@
+namespace Salvo;
+
+internal class Game 
+{
+    private readonly IReadWrite _io;
+    private readonly IRandom _random;
+
+    public Game(IReadWrite io, IRandom random)
+    {
+        _io = io;
+        _random = random;
+    }
+
+    internal void Play()
+    {
+        _io.Write(Streams.Title);
+    }
+}
---
+++ 77_Salvo/csharp/Program.cs
@@ -0,0 +1,6 @@
+global using Games.Common.IO;
+global using Games.Common.Randomness;
+global using static Salvo.Resources.Resource;
+using Salvo;
+
+new Game(new ConsoleIO(), new RandomNumberGenerator()).Play();
