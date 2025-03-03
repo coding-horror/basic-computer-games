2f0ad3cac987ba382b8a77c4319af7e2a7748618
marquesrs <80490672+marquesrs@users.noreply.github.com>
2025-03-03 12:52:49 -0300

    depth charge game initial rust implementation

--- 31_Depth_Charge/rust/src/main.rs
+++ 31_Depth_Charge/rust/src/main.rs
@@ -1,5 +1,7 @@
 use std::io::Write;
 
+use rand::Rng;
+
 /** DEPTH CHARGE GAME 
  * https://github.com/marquesrs/basic-computer-games/blob/main/31_Depth_Charge/depthcharge.bas
  * Direct conversion from BASIC to Rust by Pablo Marques (marquesrs).
@@ -28,10 +30,10 @@ fn main() {
     ));
 
     // 20 INPUT "DIMENSION OF SEARCH AREA";G: PRINT
-    let mut g = input("DIMENSION OF SEARCH AREA");
+    let g = input("DIMENSION OF SEARCH AREA: ").parse::<i32>().unwrap();
 
     // 30 N=INT(LOG(G)/LOG(2))+1
-    let mut n = INT(LOG(G)/LOG(2)) + 1;
+    let n = (f32::ln(g as f32) / f32::ln(2.0) + 1.0) as i32;
 
     // 40 PRINT "YOU ARE THE CAPTAIN OF THE DESTROYER USS COMPUTER"
     // 50 PRINT "AN ENEMY SUB HAS BEEN CAUSING YOU TROUBLE.  YOUR"
@@ -40,54 +42,50 @@ fn main() {
     // 80 PRINT "TRIO OF NUMBERS -- THE FIRST TWO ARE THE"
     // 90 PRINT "SURFACE COORDINATES; THE THIRD IS THE DEPTH."
     // 100 PRINT : PRINT "GOOD LUCK !": PRINT
-    print!("{}", format!("{}{}{}{}{}{}{}{}{}",
-        "YOU ARE THE CAPTAIN OF THE DESTROYER USS COMPUTER\n",
-        "AN ENEMY SUB HAS BEEN CAUSING YOU TROUBLE.  YOUR\n",
-        "MISSION IS TO DESTROY IT.  YOU HAVE",
+    let a = (g * rand::rng().random_range(0..=1)) as i32;
+    let b = (g * rand::rng().random_range(0..=1)) as i32;
+    let c = (g * rand::rng().random_range(0..=1)) as i32;
+    print!("{}", format!("{}{}{}{}{}{}{}{}{}{}{}",
+        "\nYOU ARE THE CAPTAIN OF THE DESTROYER USS COMPUTER\n",
+        "AN ENEMY SUB HAS BEEN CAUSING YOU TROUBLE. YOUR\n",
+        "MISSION IS TO DESTROY IT. YOU HAVE ",
         n,
-        "SHOTS.\n",
+        " SHOTS.\n",
         "SPECIFY DEPTH CHARGE EXPLOSION POINT WITH A\n",
         "TRIO OF NUMBERS -- THE FIRST TWO ARE THE\n",
         "SURFACE COORDINATES; THE THIRD IS THE DEPTH.\n",
+        format!("EXAMPLE FOR DIMENSION {}: ", g),
+        format!("{}, {}, {}", a, b, c),
         "\nGOOD LUCK !\n"
     ));
 
     'main: loop {
         // 110 A=INT(G*RND(1)) : B=INT(G*RND(1)) : C=INT(G*RND(1))
-        let mut a = INT(G*RND(1));
-        let mut b = INT(G*RND(1));
-        let mut c = INT(G*RND(1));
-
+        let a = (g * rand::rng().random_range(0..=1)) as i32;
+        let b = (g * rand::rng().random_range(0..=1)) as i32;
+        let c = (g * rand::rng().random_range(0..=1)) as i32;
         // 120 FOR D=1 TO N : PRINT : PRINT "TRIAL #";D; : INPUT X,Y,Z
         let mut x;
         let mut y;
         let mut z;
         for d in 1..=n {
-            print!("\nTRIAL #{}", d);
-            x = input("X: ");
-            y = input("X: ");
-            z = input("X: ");
+            print!("\nTRIAL #{}\n", d);
+            x = input("X: ").parse::<i32>().unwrap();
+            y = input("Y: ").parse::<i32>().unwrap();
+            z = input("Z: ").parse::<i32>().unwrap();
             // 130 IF ABS(X-A)+ABS(Y-B)+ABS(Z-C)=0 THEN 300
             if i32::abs(x-a) + i32::abs(y-b) + i32::abs(z-c) == 0 {
                 // 300 PRINT : PRINT "B O O M ! ! YOU FOUND IT IN";D;"TRIES!"
-                // 400 PRINT : PRINT: INPUT "ANOTHER GAME (Y OR N)";A$
                 print!("{}", format!("{}{}{}{}{}",
                     "\n",
-                    "B O O M ! ! YOU FOUND IT IN",
+                    "B O O M ! ! YOU FOUND IT IN ",
                     d,
-                    "TRIES!\n",
-                    "\n\n"
+                    " TRIES!\n",
+                    "\n"
                 ));
-                let a = input("ANOTHER GAME (Y OR N): ");
-                // 410 IF A$="Y" THEN 100
-                if a == "Y" {
-                    continue 'main;
-                }
-                else {
-                    // 420 PRINT "OK.  HOPE YOU ENJOYED YOURSELF." : GOTO 600
-                    print!("OK. HOPE YOU ENJOYED YOURSELF.");
-                    break 'main; 
-                }
+                // 400 PRINT : PRINT: INPUT "ANOTHER GAME (Y OR N)";A$
+                if replay() { continue 'main; }
+                else { break 'main; }
             }
         
             // 140 GOSUB 500 : PRINT : NEXT D
@@ -96,15 +94,18 @@ fn main() {
         }
         // 200 PRINT : PRINT "YOU HAVE BEEN TORPEDOED!  ABANDON SHIP!"
         // 210 PRINT "THE SUBMARINE WAS AT";A;",";B;",";C : GOTO 400
-        print!("{}", format!("{}{}{}{}{}{}{}",
+        print!("{}", format!("{}{}{}{}{}{}{}{}",
             "\nYOU HAVE BEEN TORPEDOED! ABANDON SHIP!\n",
             "THE SUBMARINE WAS AT ",
             a,
             ",",
             b,
             ",",
-            c 
+            c,
+            "\n"
         ));
+        
+        replay();
     }
     // 600 END
 }
@@ -129,4 +130,17 @@ fn subroutine(x: i32, y: i32, z:i32, a:i32, b:i32, c:i32) {
     if z>c { print!(" TOO LOW."); };
     if z<c { print!(" TOO HIGH."); };
     if z==c { print!(" DEPTH OK."); };
+}
+
+fn replay() -> bool {
+    let r = input("ANOTHER GAME (Y OR N): ");
+    // 410 IF A$="Y" THEN 100
+    if r == "Y" {
+        return true;
+    }
+    else {
+        // 420 PRINT "OK.  HOPE YOU ENJOYED YOURSELF." : GOTO 600
+        println!("OK. HOPE YOU ENJOYED YOURSELF.");
+        return false; 
+    }
 }
